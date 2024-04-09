c5445201838bba50ef1a1f09724928b32973d2ef
Ntholi Nkhatho <ntholi@live.com>
2024-04-09 14:06:31 +0200

    Add next-nprogress-bar package and configure progress bar in providers.tsx

--- src/app/(admin)/providers.tsx
+++ src/app/(admin)/providers.tsx
@@ -1,12 +1,38 @@
 'use client';
-import { MantineProvider } from '@mantine/core';
+import {
+  MantineColorSchemeManager,
+  MantineProvider,
+  createTheme,
+} from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import React, { Suspense } from 'react';
+import { ModalsProvider } from '@mantine/modals';
+import { AppProgressBar } from 'next-nprogress-bar';
 import { SessionProvider } from 'next-auth/react';
-import React, { PropsWithChildren } from 'react';
 
-export function Providers({ children }: PropsWithChildren<{}>) {
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const theme = createTheme({
+    breakpoints: {
+      xl: '1538px',
+    },
+  });
+
   return (
-    <MantineProvider>
-      <SessionProvider>{children}</SessionProvider>;
-    </MantineProvider>
+    <Suspense>
+      <MantineProvider forceColorScheme='dark' theme={theme}>
+        <Notifications />
+        <ModalsProvider>
+          <SessionProvider>
+            {children}
+            <AppProgressBar
+              height='3px'
+              color='#2196F3'
+              options={{ showSpinner: false }}
+              shallowRouting
+            />
+          </SessionProvider>
+        </ModalsProvider>
+      </MantineProvider>
+    </Suspense>
   );
 }
