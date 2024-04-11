23abaa6f1e2a639582ab672d5e45a953b82ebee8
Ntholi Nkhatho <ntholi@live.com>
2024-04-11 16:38:49 +0200

    Add user ID to requisition creation in actions.ts

--- src/app/(admin)/admin/requisitions/actions.ts
+++ src/app/(admin)/admin/requisitions/actions.ts
@@ -1,8 +1,13 @@
 'use server';
+import { authOptions } from '@/app/api/auth/[...nextauth]/auth';
 import prisma from '@/lib/prisma';
 import { Requisition } from '@prisma/client';
+import { getServerSession } from 'next-auth';
 
 export async function create(data: Requisition) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) throw new Error('User not found');
+  data.userId = session.user.id;
   await prisma.requisition.create({
     data,
   });
