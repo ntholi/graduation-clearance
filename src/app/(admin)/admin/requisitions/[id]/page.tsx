02cc2836143d95910529b4e8097274361a454b64
Ntholi Nkhatho <ntholi@live.com>
2024-04-11 19:03:59 +0200

    Init FieldView

--- src/app/(admin)/admin/requisitions/[id]/page.tsx
+++ src/app/(admin)/admin/requisitions/[id]/page.tsx
@@ -1,10 +1,28 @@
+import { Stack } from '@mantine/core';
 import React from 'react';
+import prisma from '@/lib/prisma';
+import { notFound } from 'next/navigation';
+import FieldView from '@/app/(admin)/components/FieldView';
 
 type Props = {
   params: {
     id: string;
   };
 };
-export default function Page({ params: { id } }: Props) {
-  return <div>{id}</div>;
+export default async function Page({ params: { id } }: Props) {
+  const item = await prisma.requisition.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!item) {
+    return notFound();
+  }
+
+  return (
+    <Stack p={'xl'}>
+      <FieldView label='Title' value={item.title} />
+    </Stack>
+  );
 }
