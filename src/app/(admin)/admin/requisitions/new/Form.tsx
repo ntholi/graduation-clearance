476132ca0b66a9d3bcd770cd4e660fc1eaef8b7d
Ntholi Nkhatho <ntholi@live.com>
2024-04-12 11:31:07 +0200

    init ItemsInput component

--- src/app/(admin)/admin/requisitions/new/Form.tsx
+++ src/app/(admin)/admin/requisitions/new/Form.tsx
@@ -1,10 +1,23 @@
 'use client';
 import FormHeader from '@/app/(admin)/components/FormHeader';
-import { Stack, TextInput, Textarea } from '@mantine/core';
+import {
+  Button,
+  Divider,
+  Flex,
+  Group,
+  NumberInput,
+  Paper,
+  Stack,
+  TextInput,
+  Textarea,
+  Title,
+  TableData,
+  Table,
+} from '@mantine/core';
 import { isNotEmpty, useForm } from '@mantine/form';
-import { Requisition } from '@prisma/client';
+import { Requisition, RequisitionItem } from '@prisma/client';
 import { useRouter } from 'next/navigation';
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
 
 type Props = {
   onSubmit: (values: Requisition) => Promise<Requisition>;
@@ -13,6 +26,7 @@ type Props = {
 export default function Form({ onSubmit }: Props) {
   const router = useRouter();
   const [pending, startTransition] = useTransition();
+  const [items, setItems] = useState<RequisitionItem[]>([]);
   const form = useForm<Requisition>({
     validate: {
       title: isNotEmpty('Title is required'),
@@ -36,7 +50,66 @@ export default function Form({ onSubmit }: Props) {
           rows={3}
           {...form.getInputProps('description')}
         />
+        <ItemsInput items={items} setItems={setItems} />
       </Stack>
     </form>
+  );
+}
+
+type ItemsInputProps = {
+  items: RequisitionItem[];
+  setItems: React.Dispatch<React.SetStateAction<RequisitionItem[]>>;
+};
+
+function ItemsInput({ items, setItems }: ItemsInputProps) {
+  const form = useForm<RequisitionItem>();
+
+  function handleSubmit() {
+    console.log(form.validate());
+    if (!form.validate().hasErrors) {
+      setItems((prev) => [...prev, form.values]);
+      form.reset();
+    }
+  }
+
+  const tableData: TableData = {
+    head: ['Description', 'Unit Price', 'Quantity'],
+    body: items.map((item) => [
+      `${item.description}`,
+      `${item.unitPrice}`,
+      `${item.quantity}`,
+    ]),
+  };
+
+  return (
+    <Paper p={'sm'} pb={'xl'} withBorder>
+      <Title order={4} fw={100} mb={5}>
+        Items
+      </Title>
+      <Divider />
+      <form>
+        <Flex mt={'lg'} justify='space-between' align={'center'}>
+          <Group>
+            <TextInput
+              placeholder='Description'
+              {...form.getInputProps('description')}
+            />
+            <NumberInput
+              placeholder='Unit Price'
+              {...form.getInputProps('unitPrice')}
+            />
+            <NumberInput
+              placeholder='Quantity'
+              {...form.getInputProps('quantity')}
+            />
+          </Group>
+          <Button variant='default' onClick={handleSubmit}>
+            Add
+          </Button>
+        </Flex>
+      </form>
+      <Divider my={15} />
+      <Table data={tableData} />
+    </Paper>
   );
 }
