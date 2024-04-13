bf3f47538a3cd2b531c55a9317a950315d4556f2
Ntholi Nkhatho <ntholi@live.com>
2024-04-13 09:19:27 +0200

    finally uploading to google drive works properly

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { google } from 'googleapis';
+import { google, drive_v3 } from 'googleapis';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]/auth';
 import { Readable } from 'stream';
@@ -8,23 +8,23 @@ const uploadToGoogleDrive = async (_file: File, accessToken: string) => {
   const oauth2Client = new google.auth.OAuth2();
   oauth2Client.setCredentials({ access_token: accessToken });
   const service = google.drive({ version: 'v3', auth: oauth2Client });
-  const requestBody = {
-    name: 'photo.jpg',
-    fields: 'id',
+
+  const fileMetadata: drive_v3.Schema$File = {
+    name: _file.name,
+    parents: ['1uB57yjnOnksslvynfgCMIw0XiAHhj_6m'],
   };
 
   const buffer = Buffer.from(await _file.arrayBuffer());
   const stream = Readable.from(buffer);
 
   const media = {
-    mimeType: 'image/jpeg',
+    mimeType: _file.type,
     body: stream,
   };
   const file = await service.files.create({
-    requestBody,
+    requestBody: fileMetadata,
     media: media,
   });
-  console.log('Uploaded:', file.data);
   return file.data.id;
 };
 
