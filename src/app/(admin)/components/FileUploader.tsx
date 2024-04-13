09797d1b6e58280e4b542aec7c88430323dfcabd
Ntholi Nkhatho <ntholi@live.com>
2024-04-13 16:22:29 +0200

    Refactor FileUploader component to include file description and improve UI

--- src/app/(admin)/components/FileUploader.tsx
+++ src/app/(admin)/components/FileUploader.tsx
@@ -1,30 +1,63 @@
 'use client';
-import { ActionIcon, FileInput, Modal } from '@mantine/core';
+import {
+  ActionIcon,
+  Button,
+  FileInput,
+  Modal,
+  Stack,
+  Text,
+  TextInput,
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { IconCloudUpload } from '@tabler/icons-react';
-import React from 'react';
+import React, { useTransition } from 'react';
 
 export default function FileUploader() {
   const [opened, { open, close }] = useDisclosure(false);
+  const [file, setFile] = React.useState<File | null>(null);
+  const [description, setDescription] = React.useState<string>('');
+  const [isPending, startTransition] = useTransition();
 
-  async function handleChange(file: File | null) {
+  async function handleUpload() {
     if (!file) return;
     const formData = new FormData();
     formData.append('file', file);
+    formData.append('description', description);
 
-    await fetch('/api/upload', {
-      method: 'POST',
-      body: formData,
+    startTransition(async () => {
+      await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      setFile(null);
+      setDescription('');
+      close();
     });
   }
 
   return (
     <>
-      <Modal opened={opened} onClose={close} title='Authentication'>
-        <FileInput label='Input label' onChange={handleChange} />
+      <Modal opened={opened} onClose={close} title='Upload Dialog'>
+        <Stack>
+          <FileInput required label='File' onChange={setFile} value={file} />
+          <TextInput
+            label='Description'
+            description='Describe the file, may be left empty.'
+            onChange={(event) => setDescription(event.currentTarget.value)}
+            value={description}
+          />
+          <Button onClick={handleUpload} loading={isPending}>
+            Upload
+          </Button>
+        </Stack>
       </Modal>
-      <ActionIcon onClick={open} variant='default'>
-        <IconCloudUpload />
+      <ActionIcon onClick={open} variant='default' h={200} w={'100%'}>
+        <Stack align='center' gap={10}>
+          <IconCloudUpload size={'2rem'} />
+          <Text size='0.9rem' c='gray'>
+            Upload
+          </Text>
+        </Stack>
       </ActionIcon>
     </>
   );
