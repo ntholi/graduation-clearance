fdd8a2f121f6e23efc420857bd93e8bf7cfa9f21
Ntholi Nkhatho <ntholi@live.com>
2024-04-15 05:49:46 +0200

    init Row component

--- src/app/(admin)/admin/requisitions/[id]/RequisitionPrint.tsx
+++ src/app/(admin)/admin/requisitions/[id]/RequisitionPrint.tsx
@@ -1,5 +1,14 @@
-import React, { forwardRef } from 'react';
-import { Title } from '@mantine/core';
+import React, { PropsWithChildren, forwardRef } from 'react';
+import {
+  Box,
+  BoxProps,
+  Grid,
+  GridCol,
+  GridProps,
+  Group,
+  Title,
+  Text,
+} from '@mantine/core';
 import { Requisition, RequisitionItem } from '@prisma/client';
 
 type Props = {
@@ -9,13 +18,33 @@ type Props = {
 const RequisitionPrint = forwardRef<HTMLDivElement, Props>(
   ({ requisition, ...props }, ref) => {
     return (
-      <div ref={ref} {...props}>
-        <Title fw={'lighter'} ta={'center'}>
-          {requisition.title}
-        </Title>
-      </div>
+      <Box p='xl' ref={ref} {...props}>
+        <Row label='Campus' value='Maseru' />
+      </Box>
     );
   }
 );
+
+type RowProps = {
+  label: string;
+  value: string;
+} & GridProps;
+
+function Row({ label, value, ...props }: RowProps) {
+  return (
+    <Grid {...props} gutter={5}>
+      <GridCol span={4} style={{ border: '1px solid black' }} p='xs'>
+        <Text size='12px'>{label}</Text>
+      </GridCol>
+      <GridCol
+        span={8}
+        style={{ border: '1px solid black', borderLeftWidth: 0 }}
+        p='sm'
+      >
+        <Text size='12px'>{value}</Text>
+      </GridCol>
+    </Grid>
+  );
+}
 
 export default RequisitionPrint;
