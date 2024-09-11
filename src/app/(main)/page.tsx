d3066c44689f9d12c48432bff54c902cc76fe516
Ntholi Nkhatho <ntholi@live.com>
2024-09-11 18:24:41 +0200

    Simplify home page design

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,12 @@
 import { auth } from '@/auth';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   Card,
+  CardDescription,
   CardHeader,
   CardTitle,
-  CardDescription,
 } from '@/components/ui/card';
 import Container from '@/components/ui/container';
-import { User, BookOpen, GraduationCap, FileText } from 'lucide-react';
+import { FileText, GraduationCap } from 'lucide-react';
 import Link from 'next/link';
 
 export default async function Home() {
@@ -15,12 +14,6 @@ export default async function Home() {
   const user = session?.user;
 
   const links = [
-    {
-      title: 'Register',
-      description: 'Register for 2024 term',
-      href: '/register',
-      icon: BookOpen,
-    },
     {
       title: 'Graduation',
       description: 'Clearance for Graduation',
@@ -37,34 +30,26 @@ export default async function Home() {
 
   return (
     <Container className='mt-10'>
-      <div className='flex items-center mb-8 space-x-4'>
-        <Avatar className='size-24'>
-          <AvatarImage src={user?.image || undefined} />
-          <AvatarFallback>
-            <User className='size-12' />
-          </AvatarFallback>
-        </Avatar>
-        <div>
-          <h1 className='text-3xl font-bold'>{user?.name || 'Student Name'}</h1>
-          <p className='text-lg text-gray-600'>
-            Student Number: {user?.id || '12345678'}
-          </p>
-          <p className='text-lg text-gray-600'>
-            Course of Study: Computer Science
-          </p>
-        </div>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-3xl sm:text-5xl'>{user?.name || 'Student Name'}</h1>
+        <p className='text-muted-foreground'>901013847</p>
+        <p className='text-muted-foreground text-center'>
+          Bsc Hons in Business Information Technology
+        </p>
       </div>
 
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-10'>
         {links.map((link) => (
           <Link key={link.title} href={link.href}>
             <Card className='hover:shadow-lg transition-shadow'>
               <CardHeader>
                 <div className='flex items-center space-x-4'>
                   <link.icon className='size-8 text-primary' />
-                  <div>
+                  <div className='flex flex-col gap-1'>
                     <CardTitle>{link.title}</CardTitle>
-                    <CardDescription>{link.description}</CardDescription>
+                    <CardDescription className='text-[0.8rem]'>
+                      {link.description}
+                    </CardDescription>
                   </div>
                 </div>
               </CardHeader>
