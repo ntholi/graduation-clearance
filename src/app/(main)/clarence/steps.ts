0b788c46c49c13c3cc1eb33b452b88c892f111b1
Ntholi Nkhatho <ntholi@live.com>
2024-09-12 15:04:45 +0200

    redo descriptions

--- src/app/(main)/clarence/steps.ts
+++ src/app/(main)/clarence/steps.ts
@@ -17,27 +17,25 @@ export const steps: Step[] = [
   {
     id: 1,
     title: 'Faculty',
-    description:
-      'Cleared from faculty, no pending repeat modules and GPA above 2.0',
+    description: 'No pending repeat modules and GPA above 2.0',
     icon: Users,
   },
   {
     id: 2,
     title: 'Library',
-    description: 'You should not have any unreturned books',
+    description: 'No unreturned books or fines',
     icon: BookOpen,
   },
   {
     id: 3,
     title: 'Resource Department',
-    description:
-      'All resources should be returned, and no pending dues to the resource department',
+    description: 'No unreturned resources or fines',
     icon: Building2,
   },
   {
     id: 4,
     title: 'Finance',
-    description: 'Cleared from finance, no pending dues',
+    description: 'No pending dues',
     icon: DollarSign,
   },
 ];
