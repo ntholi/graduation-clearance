cb654e19de527b30ca1f682552315534002c3dee
Ntholi Nkhatho <ntholi@live.com>
2024-09-12 19:52:09 +0200

    Init Login page

--- src/app/(main)/base/Logo.tsx
+++ src/app/(main)/base/Logo.tsx
@@ -1,23 +1,25 @@
+'use client';
+
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import Link from 'next/link';
 
 type Props = {
   className?: string;
+  width?: number;
+  height?: number;
 };
 
-export default function Logo({ className }: Props) {
+export default function Logo({ className, width = 100, height = 100 }: Props) {
   const { theme } = useTheme();
 
   return (
-    <Link href='/' className={className}>
-      <Image
-        width={100}
-        height={100}
-        className={className}
-        src={theme === 'dark' ? '/logo-white.png' : '/logo-black.png'}
-        alt='Logo'
-      />
-    </Link>
+    <Image
+      width={width}
+      height={height}
+      className={className}
+      src={theme === 'dark' ? '/logo-white.png' : '/logo-black.png'}
+      alt='Logo'
+    />
   );
 }
--- src/app/(main)/base/Navbar.tsx
+++ src/app/(main)/base/Navbar.tsx
@@ -2,15 +2,17 @@
 import Container from '@/components/ui/container';
 import Logo from './Logo';
 import UserButton from './UserButton';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
     <nav className='border-b p-2'>
       <Container width='lg' className='flex items-center justify-between'>
         <div className='flex items-center'>
-          <Logo className='mr-4 h-14 w-auto' />
+          <Link href='/'>
+            <Logo className='mr-4 h-14 w-auto' />
+          </Link>
         </div>
-
         <UserButton />
       </Container>
     </nav>
