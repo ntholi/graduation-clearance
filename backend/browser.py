2f4c8c3b67c522856b9d4a1b1a562819d10dfb99
Ntholi Nkhatho <ntholi@live.com>
2024-09-14 13:17:46 +0200

    Add browser class

---
+++ backend/browser.py
@@ -0,0 +1,108 @@
+import logging
+from urllib.parse import quote_plus
+
+import requests
+import urllib3
+from bs4 import BeautifulSoup, Tag
+from requests import Response
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from urllib3.exceptions import InsecureRequestWarning
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+BASE_URL = "https://cmslesotho.limkokwing.net/campus/registry"
+
+urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def get_form_payload(form: Tag):
+    data = {}
+    inputs = form.select("input")
+    for tag in inputs:
+        if tag.attrs["type"] == "hidden":
+            data[tag.attrs["name"]] = tag.attrs["value"]
+    return data
+
+
+def check_logged_in(html: str) -> bool:
+    page = BeautifulSoup(html, "lxml")
+    form = page.select_one("form")
+    if form:
+        if form.attrs.get("action") == "login.php":
+            return False
+    return True
+
+
+class Browser:
+    _instance = None
+    logged_in = False
+    session: requests.Session | None = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Browser, cls).__new__(cls)
+            cls._instance.session = requests.Session()
+            cls._instance.session.verify = False
+        return cls._instance
+
+    def login(self):
+        logger.info("Logging in...")
+        driver = webdriver.Firefox()
+        url = f"{BASE_URL}/login.php"
+        logger.info(f"Fetching {url}")
+        driver.get(url)
+        WebDriverWait(driver, 60 * 3).until(
+            expected_conditions.presence_of_element_located(
+                (By.LINK_TEXT, "[ Logout ]")
+            )
+        )
+        logger.info("Logged in successfully")
+
+        selenium_cookies = driver.get_cookies()
+        driver.quit()
+
+        if self.session is None:
+            raise ValueError("Session is not initialized")
+
+        self.session.cookies.clear()
+        for cookie in selenium_cookies:
+            self.session.cookies.set(
+                cookie["name"], cookie["value"], domain=cookie["domain"]
+            )
+
+    def fetch(self, url: str) -> Response:
+        if self.session is None:
+            raise ValueError("Session is not initialized")
+        logger.info(f"Fetching {url}")
+        response = self.session.get(url, timeout=60)
+        is_logged_in = check_logged_in(response.text)
+        if not is_logged_in:
+            logger.info("Session expired, logging in again")
+            self.login()
+            logger.info(f"Logged in, re-fetching {url}")
+            response = self.session.get(url)
+        if response.status_code != 200:
+            logger.warning(f"Unexpected status code: {response.status_code}")
+        return response
+
+    def post(self, url: str, data: dict | str) -> Response:
+        if self.session is None:
+            raise ValueError("Session is not initialized")
+        logger.info(f"Posting to {url}")
+        logger.info(f"Data: {data}")
+        response = self.session.post(url, data, timeout=120)
+        is_logged_in = check_logged_in(response.text)
+        if not is_logged_in:
+            logger.info("Not logged in")
+            self.login()
+            logger.info(f"Logged in, re-posting to {url}")
+            response = self.session.post(url, data, timeout=120)
+        if response.status_code != 200:
+            logger.warning(f"Unexpected status code: {response.status_code}")
+        return response
