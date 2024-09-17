c73d8b3a4596dec0e9ff360bdc242b0eac96931c
Ntholi Nkhatho <ntholi@live.com>
2024-09-17 09:17:49 +0200

    remove unused admin layout components

--- src/app/(admin)/core/RecordsPage.tsx
+++ src/app/(admin)/core/RecordsPage.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 export default function RecordsPage({ children, title }: Props) {
   return (
-    <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+    <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-10'>
       <div className='flex items-center'>
         <h1 className='text-lg font-semibold md:text-2xl'>{title}</h1>
       </div>
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -47,10 +47,6 @@ export default function AdminLayout({ children }: Props) {
             <Link href='/' className='flex items-center gap-2 font-semibold'>
               <Logo className='h-11 w-auto' width={150} height={150} />
             </Link>
-            <Button variant='outline' size='icon' className='ml-auto h-8 w-8'>
-              <Bell className='h-4 w-4' />
-              <span className='sr-only'>Toggle notifications</span>
-            </Button>
           </div>
           <div className='flex-1'>
             <nav className='grid items-start px-2 text-sm font-medium lg:px-4'>
@@ -169,34 +165,6 @@ export default function AdminLayout({ children }: Props) {
               </div>
             </SheetContent>
           </Sheet>
-          <div className='w-full flex-1'>
-            <form>
-              <div className='relative'>
-                <Search className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
-                <Input
-                  type='search'
-                  placeholder='Search products...'
-                  className='w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3'
-                />
-              </div>
-            </form>
-          </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant='secondary' size='icon' className='rounded-full'>
-                <CircleUser className='h-5 w-5' />
-                <span className='sr-only'>Toggle user menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end'>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
         </header>
         {children}
       </div>
