adf304fde7dd183cd9fa0006496f9fa17889641b
Ntholi Nkhatho <ntholi@live.com>
2024-09-25 15:42:06 +0200

    switch to local database

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,13 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+// import { neon } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
+import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
 
-const sql = neon(process.env.AUTH_DRIZZLE_URL!);
-const db = drizzle(sql, { schema });
+const queryClient = postgres(process.env.AUTH_DRIZZLE_URL!);
+const db = drizzle(queryClient);
+
+// const sql = neon(process.env.AUTH_DRIZZLE_URL!);
+// const db = drizzle(sql, { schema });
 
 export default db;
