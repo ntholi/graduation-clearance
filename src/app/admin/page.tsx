44a4766d32f3fbfcdd6718abe6482832cdf929a3
Ntholi Nkhatho <ntholi@live.com>
2024-09-28 07:39:46 +0200

    Make a proper admin home page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,25 @@
-import { Center, Text } from '@mantine/core';
+import { auth } from '@/auth';
+import { Stack, Title, Text, Anchor, Flex } from '@mantine/core';
+import React, { Suspense } from 'react';
 
-export default function MainPage() {
+export default function AdminPage() {
   return (
-    <Center>
-      <Text c='dimmed'>Admin</Text>
-    </Center>
+    <Stack h={'70vh'} w={'100%'} justify='center' align='center'>
+      <div>
+        <Title fw={'lighter'}>Admin Panel</Title>
+        <Suspense fallback={<Text>...</Text>}>
+          <UserDisplay />
+        </Suspense>
+      </div>
+    </Stack>
+  );
+}
+
+async function UserDisplay() {
+  const session = await auth();
+  return (
+    <Text size='sm' mt='xs'>
+      Welcome, {session?.user?.name}
+    </Text>
   );
 }
