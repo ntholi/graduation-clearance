d9cbe478ba4a2d7032fbe68e786d5c748e0dd000
Ntholi Nkhatho <ntholi@live.com>
2024-11-06 15:57:50 +0200

    increase cleared-students table width

--- src/app/admin/reports/cleared-students/page.tsx
+++ src/app/admin/reports/cleared-students/page.tsx
@@ -83,7 +83,7 @@ export default function Page() {
         </Flex>
       </Paper>
       <Paper withBorder p='md'>
-        <Table.ScrollContainer minWidth={1300}>
+        <Table.ScrollContainer minWidth={1500}>
           <Table>
             <TableThead>
               <TableTr>
